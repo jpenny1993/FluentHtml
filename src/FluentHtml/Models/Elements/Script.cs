885566ad8bde5393ef68fbc81f7b552aaa4bd19b
John Penny <jpenny1993@gmail.com>
2018-09-23 17:35:20 +0100

    Add script tag

---
+++ src/FluentHtml/Models/Elements/Script.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentHtml.Models.Elements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// The <script> element is used to define client-side JavaScripts.
+    /// </summary>
+    public class Script : Node
+    {
+        public Script() : base("script")
+        {
+        }
+    }
+}
