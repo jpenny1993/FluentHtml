eebf4bb4d4f1fd0d93eba88fb731c909551beb0c
John Penny <jpenny1993@gmail.com>
2018-09-23 17:35:37 +0100

    fix indenting on inner text

--- src/FluentHtml/Models/InnerText.cs
+++ src/FluentHtml/Models/InnerText.cs
@@ -1,18 +1,29 @@
 ﻿namespace FluentHtml.Models
 {
+    using System;
     using System.Text;
+    using FluentHtml.Extensions;
 
     public sealed class InnerText : Node
     {
         public string Content { get; set; }
 
-        public InnerText() : base()
+        public InnerText() : base(string.Empty, isSingleTag: true)
         {
         }
         
         public override StringBuilder Draw(StringBuilder builder, int indent)
         {
-            return builder.Append(Content);
+            var lines = Content?.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries) 
+                ?? new string[0];
+
+            foreach (var line in lines)
+            {
+                builder.DrawIndent(indent);
+                builder.AppendLine(line);
+            }
+
+            return builder;
         }
     }
 }
