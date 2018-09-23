af992d5019bfbea79bc2e5c3f430995cf0eca9fc
John Penny <jpenny1993@gmail.com>
2018-09-23 17:35:57 +0100

    change node extensions

--- src/FluentHtml/Extensions/NodeExtensions.cs
+++ src/FluentHtml/Extensions/NodeExtensions.cs
@@ -2,25 +2,59 @@
 {
     using System;
     using FluentHtml.Interfaces;
+    using FluentHtml.Models;
+    using FluentHtml.Models.Attributes;
 
     public static class NodeExtensions
     {
-        public static INode AddElement<TElement>(this INode node)
-               where TElement : INode
+        public static INode AddAttribute(this INode node, string name, string value)
         {
-            return node.Begin<TElement>().End();
+            return node.AddAttribute(new HtmlAttribute
+            {
+                Name = name,
+                Value = value
+            });
         }
 
-        public static TElement Begin<TElement>(this INode node)
+        public static INode AddAttribute<TAttribute>(this INode node, TAttribute attribute)
+               where TAttribute : IAttribute
+        {
+            node.Attributes.Add(attribute);
+            return node;
+        }
+
+        public static INode AddElement<TElement>(this INode node, TElement element)
+               where TElement : INode
+        {
+            element.Parent = node;
+            node.Children.Add(element);
+            return node;
+        }
+
+        public static INode AddElement<TElement>(this INode node)
+              where TElement : INode
+        {
+            var element = Activator.CreateInstance<TElement>();
+            return node.AddElement(element);
+        }
+
+        public static INode AddInnerText(this INode node, string text)
+        {
+            return node.AddElement(new InnerText
+            {
+                Content = text
+            });
+        } 
+
+        public static TElement BeginElement<TElement>(this INode node)
                where TElement : INode
         {
             var element = Activator.CreateInstance<TElement>();
-            element.Parent = node;
-            node.Children.Add(element);
+            node.AddElement(element);
             return element;
-        }
+        } 
 
-        public static INode End(this INode node)
+        public static INode EndElement(this INode node)
         {
             if (node.Parent == default(INode))
             {
